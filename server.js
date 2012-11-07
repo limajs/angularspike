3073bfd2539daf7e628200463b53c437a9c031fd
Simon Crabtree <limajs@simoncrabtree.net>
2012-11-07 16:46:31 +0000

    Initial Hello World

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express"),
+app = express(),
+server = require("http").createServer(app);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendfile('index.html');
+});
+
+server.listen(8000);
+console.log("Listening on port 8000");
